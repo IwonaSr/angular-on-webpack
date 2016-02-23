4022c9a36afdcef56b96d26b8d28bdd74db120d0
Bartek <bslipek@gmail.com>
2016-02-23 14:53:48 +0000

    Adds jasmine/karma tests

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@ import angular from  'angular';
 import uirouter from 'angular-ui-router';
 import routing from './config';
 
+if (ON_TEST) {
+  require('angular-mocks/angular-mocks');
+}
+
 import home from './features/home'
 import video from './features/video'
 

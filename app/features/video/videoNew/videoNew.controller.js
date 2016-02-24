13cacb0856a96671c393d9e63cf8673314f3e8a1
Bartek <bslipek@gmail.com>
2016-02-24 22:15:01 +0100

    Redirects to listing after add new video

--- app/features/video/videoNew/videoNew.controller.js
+++ app/features/video/videoNew/videoNew.controller.js
@@ -1,11 +1,13 @@
 export default class VideoNewController {
-  constructor(VideoService) {
+  constructor(VideoService, $state) {
     this.videoService = VideoService;
     this.newVideo = {};
     this.name = 'New Video Record Information';
+    this.state = $state;
   }
 
   addVideo(newVideo) {
     this.videoService.addVideo(newVideo);
+    this.state.go('videoListing');
   }
 }

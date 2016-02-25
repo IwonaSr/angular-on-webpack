c7233f2d24f8ba8c03ab7ef2cba6f6612b8efa8b
Bartek <bslipek@gmail.com>
2016-02-25 01:41:28 +0100

    Shows snapshot after click on snapshot thumbnail

--- app/features/video/videoListing/videoListing.controller.js
+++ app/features/video/videoListing/videoListing.controller.js
@@ -2,9 +2,14 @@ export default class VideosListingController {
   constructor(VideoService) {
     this.videoService = VideoService;
     this.videos = this.videoService.getVideos();
+    this.showingSnap = null;
   }
 
   deleteVideo(video){
     this.videoService.deleteVideo(video);
+  }
+
+  showSnap(url){
+    this.showingSnap = url;
   }
 }
